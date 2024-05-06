43824c886f1655b23019ab1558df562d9dda9968
Roey D. Chasman <rchasman@gmail.com>
2024-05-06 11:44:58 -0700

    RDC - Use Paragraph Indent Plugin published as its own package.

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -37,6 +37,7 @@ import { Plugin } from '@ckeditor/ckeditor5-core';
 import { Widget } from "@ckeditor/ckeditor5-widget";
 import { Mention } from '@ckeditor/ckeditor5-mention';
 import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { IndentParagraph } from '@lawmatics/ckeditor5-paragraph-indent';
 import { ImportWord } from '@ckeditor/ckeditor5-import-word';
 import MergeFieldCommand from "./commands/mergeFieldCommand";
 import * as DOMPurify from 'dompurify';
@@ -61,56 +62,6 @@ class EmTagItalicPlugin extends Plugin {
       converterPriority: 'high',
       upcastAlso: ['i', { styles: { 'font-style': 'italic' } }],
     });
-  }
-}
-
-class ParagraphIndentPlugin extends Plugin {
-  static get defaultIndentSize() {
-    return 36;
-  }
-
-  init() {
-    const editor = this.editor;
-
-    const keystrokes = [
-      { key: 'Tab', action: 'increase' },
-      { key: 'Shift+Tab', action: 'decrease' },
-      { key: 'Backspace', action: 'decrease' }
-    ];
-
-    keystrokes.forEach(({ key, action }) => {
-      editor.keystrokes.set(key, (_, cancel) => {
-        const indentChanged = this.adjustParagraphIndent(editor, action);
-        if (key !== 'Backspace' || indentChanged) {
-          cancel(); // Always cancel for Tab and Shift+Tab, or if Backspace changed the indent
-        }
-      }, { priority: 'high' });
-    });
-
-    editor.conversion.for('downcast').attributeToAttribute({
-      model: 'textIndent',
-      view: (_, _2, { attributeNewValue }) => ({ key: 'style', value: { 'text-indent': `${attributeNewValue}px` } })
-    });
-  }
-
-  adjustParagraphIndent(editor, action) {
-    const selection = editor.model.document.selection;
-    const position = selection.getFirstPosition();
-    const paragraph = position.findAncestor('paragraph');
-    if (position.isAtStart && paragraph) {
-      const viewElement = editor.editing.mapper.toViewElement(paragraph);
-      const currentIndent = parseInt(viewElement.getStyle('text-indent') || paragraph.getAttribute('textIndent') || '0');
-      const change = action === 'increase' ? ParagraphIndentPlugin.defaultIndentSize : -ParagraphIndentPlugin.defaultIndentSize;
-      const newIndentValue = Math.max(currentIndent + change, 0);
-
-      if (newIndentValue !== currentIndent) {
-        editor.model.change(writer => {
-          writer.setAttribute('textIndent', newIndentValue, paragraph);
-        });
-        return true; // Indent was adjusted
-      }
-    }
-    return false; // No indent adjustment was made
   }
 }
 
@@ -213,6 +164,7 @@ ClassicEditor.builtinPlugins = [
   ImageUpload,
   Indent,
   IndentBlock,
+  IndentParagraph,
   Link,
   LinkImage,
   List,
@@ -242,7 +194,6 @@ ClassicEditor.builtinPlugins = [
   EmTagItalicPlugin,
   TextTransformation,
   MergeFieldPlugin,
-  ParagraphIndentPlugin,
 ];
 
 // Editor configuration.
